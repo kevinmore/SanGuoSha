664a8f219332af8207763c956b37540e9dc15607
Kevin Wang <dingfengyu@gmail.com>
2023-04-06 12:11:46 +0100

    Refactor.

---
+++ lib/characters/model/character_data_mapping.dart
@@ -0,0 +1,71 @@
+import 'package:san_guo_sha/cards/model/card_value.dart';
+
+Map<CharacterCardValue, String> characterImageMap = {
+  // qun
+  CharacterCardValue.huaTuo: "assets/images/characters/华佗-经典形象.png",
+  CharacterCardValue.lvBu: "assets/images/characters/吕布-经典形象.png",
+  CharacterCardValue.diaoChan: "assets/images/characters/貂蝉-经典形象.png",
+
+  // shu
+  CharacterCardValue.liuBei: "assets/images/characters/刘备-经典形象.png",
+  CharacterCardValue.guanYu: "assets/images/characters/关羽-经典形象.png",
+  CharacterCardValue.zhangFei: "assets/images/characters/张飞-经典形象.png",
+  CharacterCardValue.zhugeLiang: "assets/images/characters/诸葛亮-经典形象.png",
+  CharacterCardValue.zhaoYun: "assets/images/characters/赵云-经典形象.png",
+  CharacterCardValue.maChao: "assets/images/characters/马超-经典形象.png",
+  CharacterCardValue.huangYueying: "assets/images/characters/黄月英-经典形象.png",
+
+  // wei
+  CharacterCardValue.caoCao: "assets/images/characters/曹操-经典形象.png",
+  CharacterCardValue.simaYi: "assets/images/characters/司马懿-经典形象.png",
+  CharacterCardValue.xiahouDun: "assets/images/characters/夏侯惇-经典形象.png",
+  CharacterCardValue.zhangLiao: "assets/images/characters/张辽-经典形象.png",
+  CharacterCardValue.xuChu: "assets/images/characters/许褚-经典形象.png",
+  CharacterCardValue.guoJia: "assets/images/characters/郭嘉-经典形象.png",
+  CharacterCardValue.zhenJi: "assets/images/characters/甄姬-经典形象.png",
+
+  // wu
+  CharacterCardValue.sunQuan: "assets/images/characters/孙权-经典形象.png",
+  CharacterCardValue.ganNing: "assets/images/characters/甘宁-经典形象.png",
+  CharacterCardValue.lvMeng: "assets/images/characters/吕蒙-经典形象.png",
+  CharacterCardValue.huangGai: "assets/images/characters/黄盖-经典形象.png",
+  CharacterCardValue.zhouYu: "assets/images/characters/周瑜-经典形象.png",
+  CharacterCardValue.daQiao: "assets/images/characters/大乔-经典形象.png",
+  CharacterCardValue.luXun: "assets/images/characters/陆逊-经典形象.png",
+  CharacterCardValue.sunShangXiang: "assets/images/characters/孙尚香-经典形象.png",
+};
+
+Map<CharacterCardValue, int> characterHealthMap = {
+  // qun
+  CharacterCardValue.huaTuo: 3,
+  CharacterCardValue.lvBu: 4,
+  CharacterCardValue.diaoChan: 3,
+
+  // shu
+  CharacterCardValue.liuBei: 4,
+  CharacterCardValue.guanYu: 4,
+  CharacterCardValue.zhangFei: 4,
+  CharacterCardValue.zhugeLiang: 3,
+  CharacterCardValue.zhaoYun: 4,
+  CharacterCardValue.maChao: 4,
+  CharacterCardValue.huangYueying: 3,
+
+  // wei
+  CharacterCardValue.caoCao: 4,
+  CharacterCardValue.simaYi: 3,
+  CharacterCardValue.xiahouDun: 4,
+  CharacterCardValue.zhangLiao: 4,
+  CharacterCardValue.xuChu: 4,
+  CharacterCardValue.guoJia: 3,
+  CharacterCardValue.zhenJi: 3,
+
+  // wu
+  CharacterCardValue.sunQuan: 4,
+  CharacterCardValue.ganNing: 4,
+  CharacterCardValue.lvMeng: 4,
+  CharacterCardValue.huangGai: 4,
+  CharacterCardValue.zhouYu: 3,
+  CharacterCardValue.daQiao: 3,
+  CharacterCardValue.luXun: 3,
+  CharacterCardValue.sunShangXiang: 3,
+};
