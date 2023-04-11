130c6e2196fdef705fc471cb4603b6f654a8ad6b
Kevin Wang <dingfengyu@gmail.com>
2023-04-11 15:53:07 +0100

    Updated the game table page.

--- lib/pages/game_table_page.dart
+++ lib/pages/game_table_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:san_guo_sha/cards/model/deck.dart';
 import 'package:san_guo_sha/cards/view/card_pile_widget.dart';
@@ -27,48 +29,52 @@ class GameTablePage extends StatelessWidget {
       DeckCard(value: DeckCardValue.two, suit: Suit.clubs, deck: 0),
     ];
 
+    int numOpponents = 7;
+    double opponentWidgetWidth = screenWidth / (numOpponents + 2);
+    double opponentWidgetHeight = min(opponentWidgetWidth / kCardAspectRatio, cardHeight);
+
     List<Widget> opponents = [
       PlayerAvatarWidget(
-        height: cardHeight,
+        height: opponentWidgetHeight,
         role: RoleCardValue.zhongChen,
         character: CharacterCardValue.diaoChan,
         health: 1,
         handCards: 5,
       ),
       PlayerAvatarWidget(
-          height: cardHeight,
+          height: opponentWidgetHeight,
           role: RoleCardValue.zhongChen,
           character: CharacterCardValue.guanYu,
           showRole: true,
           handCards: 2,
           health: 2),
       PlayerAvatarWidget(
-          height: cardHeight,
+          height: opponentWidgetHeight,
           role: RoleCardValue.zhongChen,
           character: CharacterCardValue.zhangFei,
           handCards: 1,
           health: 4),
       PlayerAvatarWidget(
-          height: cardHeight,
+          height: opponentWidgetHeight,
           role: RoleCardValue.fanZei,
           character: CharacterCardValue.guoJia,
           handCards: 0,
           health: 2),
       PlayerAvatarWidget(
-          height: cardHeight,
+          height: opponentWidgetHeight,
           role: RoleCardValue.fanZei,
           character: CharacterCardValue.huangGai,
           showRole: true,
           handCards: 1,
           health: 0),
       PlayerAvatarWidget(
-          height: cardHeight,
+          height: opponentWidgetHeight,
           role: RoleCardValue.fanZei,
           character: CharacterCardValue.huaTuo,
           handCards: 4,
           health: 3),
       PlayerAvatarWidget(
-          height: cardHeight,
+          height: opponentWidgetHeight,
           role: RoleCardValue.neiJian,
           character: CharacterCardValue.zhenJi,
           showRole: true,
@@ -80,47 +86,43 @@ class GameTablePage extends StatelessWidget {
       body: Container(
           decoration: const BoxDecoration(
             image: DecorationImage(
-              image: AssetImage("assets/images/backgrounds/魏.jpg"),
+              image: AssetImage("assets/images/backgrounds/background.jpeg"),
               fit: BoxFit.cover,
             ),
           ),
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.only(top: 16, bottom: 8, right: 16, left: 16),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 // opponents
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: opponents,
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: opponents,
                 ),
 
                 //deck cards
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.03),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      CardPileWidget(
-                        cards: deckACardsList(),
-                        cardHeight: cardHeight,
-                        cardOverlapping: screenHeight * 0.0003,
-                        isFacingDown: true,
-                        // debugMode: true,
-                      ),
-                      CardPileWidget(
-                        cards: deckBCardsList(),
-                        cardHeight: cardHeight,
-                        cardOverlapping: screenHeight * 0.0003,
-                        // debugMode: true,
-                      ),
-                    ],
-                  ),
-                ),
+                // Padding(
+                //   padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.03),
+                //   child: Row(
+                //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //     children: [
+                //       CardPileWidget(
+                //         cards: deckACardsList(),
+                //         cardHeight: cardHeight,
+                //         cardOverlapping: screenHeight * 0.0003,
+                //         isFacingDown: true,
+                //         // debugMode: true,
+                //       ),
+                //       CardPileWidget(
+                //         cards: deckBCardsList(),
+                //         cardHeight: cardHeight,
+                //         cardOverlapping: screenHeight * 0.0003,
+                //         // debugMode: true,
+                //       ),
+                //     ],
+                //   ),
+                // ),
 
                 // player
                 Row(
@@ -132,13 +134,12 @@ class GameTablePage extends StatelessWidget {
                         handCards: 6,
                         showRole: true,
                         health: 5),
-                    SizedBox(
-                        width: MediaQuery.of(context).size.width -
-                            cardHeight * kCardAspectRatio * 1.2,
-                        child: FlatCardFanWidget(
-                          cards: handCards,
-                          cardHeight: cardHeight,
-                        )),
+                    // SizedBox(
+                    //     width: MediaQuery.of(context).size.width,
+                    //     child: FlatCardFanWidget(
+                    //       cards: handCards,
+                    //       cardHeight: cardHeight,
+                    //     )),
                   ],
                 )
               ],
